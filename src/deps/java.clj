50a3ef2404b3606112e7c52d3d4663209050ff98
Lei Wang <tendant@gmail.com>
2021-03-10 13:27:40 -0800

    rename to javac

--- src/deps/java.clj
+++ src/deps/java.clj
@@ -1,13 +1,13 @@
 (ns deps.java
-   (:refer-clojure :exclude [compile])
-   (:require [badigeon.javac :as javac]
-             [badigeon.classpath :as classpath]))
+  ;; (:refer-clojure :exclude [compile])
+  (:require [badigeon.javac :as bjavac]
+            [badigeon.classpath :as classpath]))
 
-(defn compile [{:keys [source-path output-path aliases]
+(defn javac [{:keys [source-path output-path aliases]
                 :or {aliases []}}]
-  (javac/javac source-path
-               {:compile-path output-path
-                :javac-options ["-cp" (classpath/make-classpath {:aliases aliases})
-                                "-target" "8" "-source" "8"
-                                "-Xlint:-options"]})
+  (bjavac/javac source-path
+                {:compile-path output-path
+                 :javac-options ["-cp" (classpath/make-classpath {:aliases aliases})
+                                 "-target" "8" "-source" "8"
+                                 "-Xlint:-options"]})
   (println "Java Compilation Completed"))
