3da75d4e232d6e19ff0ad3de718ce48d6cf54d4c
Lei Wang <tendant@gmail.com>
2021-03-10 13:41:01 -0800

    add args

--- src/deps/java.clj
+++ src/deps/java.clj
@@ -3,11 +3,15 @@
   (:require [badigeon.javac :as bjavac]
             [badigeon.classpath :as classpath]))
 
-(defn javac [{:keys [source-path output-path aliases]
-                :or {aliases []}}]
+(defn javac [{:keys [source-path output-path source-version target-version aliases]
+                :or {aliases []
+                     source-path "java"
+                     output-path "target/classes"
+                     source-version "8"
+                     target-version "8"}}]
   (bjavac/javac source-path
                 {:compile-path output-path
                  :javac-options ["-cp" (classpath/make-classpath {:aliases aliases})
-                                 "-target" "8" "-source" "8"
+                                 "-target" target-version "-source" source-version
                                  "-Xlint:-options"]})
   (println "Java Compilation Completed"))
